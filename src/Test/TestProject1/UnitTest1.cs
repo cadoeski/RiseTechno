7c607894d99eae93d737e9cca66affbd85b4691d
çağdaş eskioğlu <ceskioglu@ulusalfaktoring.com>
2022-11-01 16:54:48 +0300

    unit test

--- src/Test/TestProject1/UnitTest1.cs
+++ src/Test/TestProject1/UnitTest1.cs
@@ -17,6 +17,9 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Report.Application.Features.LocationReport;
 using Report.Api.Controllers;
+using System;
+using Contact.Application.Features.Users.Queries.GetContacsById;
+using Report.Application.Features.Queries;
 
 namespace TestProject1
 {
@@ -72,6 +75,85 @@ namespace TestProject1
 
         }
 
+        [InlineData("123","123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789", "test", "test2")]
+        [InlineData("123", "Þirket Adý", "Name", "surName")]
+        [InlineData("123", "Þirket Adý", "name", "U")]
+        [InlineData("123", "Þirket Adý", "n", "surname")]
+        [InlineData("123", "Þirket Adý", "n", "s")]
+        [Theory]
+        public async Task UpdateUser_ShouldReturn200(Guid i,string c, string n, string s)
+        {
+            /// Arrange
+            var servie = new Mock<MediatR.IMediator>();
+            var contextAc = new Mock<IHttpContextAccessor>();
+
+
+            UserUpdateCommand user = new UserUpdateCommand();
+            user.company = c;
+            user.name = n;
+            user.surname = s;
+            user.uid = i;
+
+            var sut = new ContactController(servie.Object, contextAc.Object);
+
+            /// Act
+            var result = await sut.UserUpdate(user) as OkObjectResult;
+
+
+            // /// Assert 
+
+            Assert.True(result.StatusCode.Value == 200);
+            result.Should().NotBeNull();
+
+        }
+
+        [InlineData("123")] 
+        [Theory]
+        public async Task UpdateDelete_ShouldReturn200(string i)
+        {
+            /// Arrange
+            var servie = new Mock<MediatR.IMediator>();
+            var contextAc = new Mock<IHttpContextAccessor>();
+
+
+            UserDeleteCommand user = new UserDeleteCommand(); 
+            user.uid = i;
+
+            var sut = new ContactController(servie.Object, contextAc.Object);
+
+            /// Act
+            var result = await sut.UserDelete(user) as OkObjectResult;
+
+
+            // /// Assert 
+
+            Assert.True(result.StatusCode.Value == 200);
+            result.Should().NotBeNull();
+
+        }
+
+
+        [Fact]
+        public async Task GetUser_ShouldReturn200()
+        {
+            /// Arrange
+            var servie = new Mock<MediatR.IMediator>();
+            var contextAc = new Mock<IHttpContextAccessor>();
+
+
+            ReadContacsReport user = new ReadContacsReport(Guid.NewGuid()); 
+
+            var sut = new ContactController(servie.Object, contextAc.Object);
+
+            /// Act
+            var result = await sut.GetUser(user) as OkObjectResult;
+             
+            // /// Assert 
+
+            Assert.True(result.StatusCode.Value == 200);
+            result.Should().NotBeNull();
+
+        }
 
         [Fact]
 
@@ -95,11 +177,33 @@ namespace TestProject1
             Assert.NotNull(model);
 
             var actual = model.status;
-            Assert.Equal("Hazýrlanýyor", actual);
-             
+            Assert.Equal("Hazýrlanýyor", actual); 
 
         }
 
+        [Fact] 
+        public async Task GetReportStatus_Count()
+        {
+            /// Arrange
+            var servie = new Mock<MediatR.IMediator>();
+            var contextAc = new Mock<IHttpContextAccessor>();
+
+
+            LocationStatusGetReport report = new LocationStatusGetReport();
+            report.reportId = Guid.NewGuid();
+
+            var sut = new ReportController(servie.Object, contextAc.Object);
+
+            /// Act
+            var result = await sut.GetReportStatus(report) as OkObjectResult;
+            Assert.NotNull(result);
+             
+            var model = result.Value as LocationStatusReport;
+            Assert.NotNull(model);
+
+            var actual = model.status;
+            Assert.Equal("Hazýrlanýyor", actual); 
+        }
 
     }
 
